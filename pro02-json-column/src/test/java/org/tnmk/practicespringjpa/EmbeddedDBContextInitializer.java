e65d1d1f6100bc11a0016aed4108ca200a261b7d
Kevin Tran <kevin.tran@leonardo.com>
2018-07-13 14:27:49 -0400

    Can connect to embedded Postgres now, but cannot work with json column.

--- pro02-json-column/src/test/java/org/tnmk/practicespringjpa/EmbeddedDBContextInitializer.java
+++ pro02-json-column/src/test/java/org/tnmk/practicespringjpa/EmbeddedDBContextInitializer.java
@@ -1,18 +1,20 @@
 package org.tnmk.practicespringjpa;
 
 import com.sun.istack.internal.NotNull;
+import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 
 public class EmbeddedDBContextInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
 
     public void initialize(@NotNull final ConfigurableApplicationContext configurableApplicationContext) {
-//        TestPropertyValues.of(
-//            "spring.datasource.url=" + embeddedPostgres. + "?useSSL=false",
-//            "spring.datasource.username=" + MY_SQL_CONTAINER.getUsername(),
+//        String propertyUrl = "spring.datasource.url=" + EmbeddedDBStarter.EMBEDDED_POSTGRES.getJdbcUrl("postgres","practice_spring_jpa_db");
+        String propertyUrl = "spring.datasource.url=" + EmbeddedDBStarter.EMBEDDED_POSTGRES.getJdbcUrl("postgres","postgres");
+        TestPropertyValues.of(propertyUrl
+//            , "spring.datasource.username=" + MY_SQL_CONTAINER.getUsername(),
 //            "spring.datasource.password=" + MY_SQL_CONTAINER.getPassword(),
 //            "spring.datasource.schema-username=" + MY_SQL_CONTAINER.getUsername(),
 //            "spring.datasource.schema-password=" + MY_SQL_CONTAINER.getPassword()
-//        ).applyTo(configurableApplicationContext.getEnvironment());
+        ).applyTo(configurableApplicationContext.getEnvironment());
     }
 }
--- pro02-json-column/src/test/java/org/tnmk/practicespringjpa/JsonColumnAppTest.java
+++ pro02-json-column/src/test/java/org/tnmk/practicespringjpa/JsonColumnAppTest.java
@@ -1,27 +1,23 @@
 package org.tnmk.practicespringjpa;
 
-import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
-import org.flywaydb.test.annotation.FlywayTest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.tnmk.practicespringjpa.samplebusiness.story.SampleStory;
-
-import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
 
 @ActiveProfiles("test")
 @RunWith(SpringRunner.class)
 //@AutoConfigureEmbeddedDatabase(beanName = "dataSource")
-@AutoConfigureTestDatabase(replace = NONE)
-@AutoConfigureEmbeddedDatabase
-@FlywayTest
-@DataJpaTest
+//@AutoConfigureTestDatabase(replace = NONE)
+//@AutoConfigureEmbeddedDatabase
+//@FlywayTest
+//@DataJpaTest
 @SpringBootTest(classes = {JsonColumnApplication.class})
+@ContextConfiguration(initializers = EmbeddedDBContextInitializer.class)
 public class JsonColumnAppTest {
 //
 //    @Bean
