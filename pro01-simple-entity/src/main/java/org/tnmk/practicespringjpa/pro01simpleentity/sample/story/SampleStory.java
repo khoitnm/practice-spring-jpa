12c845bb689e5bda370b29f046a03155903f789e
Kevin Tran <kevin.tran@leonardo.com>
2019-02-06 15:33:35 -0500

    Add JdbcTemplate

--- pro01-simple-entity/src/main/java/org/tnmk/practicespringjpa/pro01simpleentity/sample/story/SampleStory.java
+++ pro01-simple-entity/src/main/java/org/tnmk/practicespringjpa/pro01simpleentity/sample/story/SampleStory.java
@@ -3,18 +3,27 @@ package org.tnmk.practicespringjpa.pro01simpleentity.sample.story;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.tnmk.practicespringjpa.pro01simpleentity.sample.entity.SampleEntity;
+import org.tnmk.practicespringjpa.pro01simpleentity.sample.entity.SampleEntityWithUrl;
+import org.tnmk.practicespringjpa.pro01simpleentity.sample.repository.SampleJdbcRepostiory;
 import org.tnmk.practicespringjpa.pro01simpleentity.sample.repository.SampleRepository;
-import org.tnmk.practicespringjpa.pro01simpleentity.sample.entity.SampleEntity;
-import org.tnmk.practicespringjpa.pro01simpleentity.sample.repository.SampleRepository;
+
+import java.util.List;
 
 @Service
 public class SampleStory {
     @Autowired
     private SampleRepository sampleRepository;
 
+    @Autowired
+    private SampleJdbcRepostiory sampleJdbcRepostiory;
+
     public SampleEntity createSample(){
         SampleEntity sampleEntity = new SampleEntity();
         sampleEntity.setName("AAA");
         return sampleRepository.save(sampleEntity);
+    }
+
+    public List<SampleEntityWithUrl> findSampleEntitiesWithUrl(){
+        return sampleJdbcRepostiory.findSampleEntitiesWithUrl();
     }
 }
