765fffd616bfab48cc62afd7d5db89183b83d1ba
Khoi Tran <khoi.tnm@gmail.com>
2019-02-06 15:33:35 -0500

    Add JdbcTemplate

--- pro01-simple-entity/src/main/java/org/tnmk/practicespringjpa/pro01simpleentity/sample/repository/SampleRepository.java
+++ pro01-simple-entity/src/main/java/org/tnmk/practicespringjpa/pro01simpleentity/sample/repository/SampleRepository.java
@@ -1,6 +1,7 @@
 package org.tnmk.practicespringjpa.pro01simpleentity.sample.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.tnmk.practicespringjpa.pro01simpleentity.sample.entity.SampleEntity;
 
 public interface SampleRepository extends JpaRepository<SampleEntity, Long> {
