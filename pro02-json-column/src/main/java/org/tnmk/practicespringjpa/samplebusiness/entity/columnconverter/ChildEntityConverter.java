a8d7b8b518377c405f6a787ece7f570505fd7204
Khoi Tran <khoi.tnm@gmail.com>
2019-03-07 13:37:00 -0500

    Create pro02-simple-ddl-generator

--- pro02-json-column/src/main/java/org/tnmk/practicespringjpa/samplebusiness/entity/columnconverter/ChildEntityConverter.java
+++
@@ -1,7 +0,0 @@
-package org.tnmk.practicespringjpa.samplebusiness.entity.columnconverter;
-
-import org.tnmk.common.jpa.columnconverter.json.JsonConverter;
-import org.tnmk.practicespringjpa.samplebusiness.entity.ChildEntity;
-
-public class ChildEntityConverter extends JsonConverter<ChildEntity> {
-}
