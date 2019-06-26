e37854a775b6fd27b027b782b35aebba45a641e3
Kevin Tran <kevin.tran@leonardo.com>
2019-06-26 12:59:34 -0400

    Change to MySQL, use TIMESTAMP(6) for storing nanosecond.

--- pro01-simple-timestamp-fields/src/test/groovy/org/tnmk/common/jpa/columnconverter/json/BaseSpecification.groovy
+++ pro01-simple-timestamp-fields/src/test/groovy/org/tnmk/common/jpa/columnconverter/json/BaseSpecification.groovy
@@ -3,12 +3,15 @@ package org.tnmk.common.jpa.columnconverter.json
 import org.junit.Ignore
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.annotation.DirtiesContext
+import org.springframework.test.context.ContextConfiguration
+import org.tnmk.common.jpa.embeddedMySQL.EmbeddedDBContextInitializer
 import org.tnmk.practicespringjpa.pro01simpletimestampfields.Application
 import spock.lang.Specification
 
 @Ignore
 @DirtiesContext
 @SpringBootTest(classes = Application.class)
+@ContextConfiguration(initializers = EmbeddedDBContextInitializer.class)
 abstract class BaseSpecification extends Specification {
 
 }
