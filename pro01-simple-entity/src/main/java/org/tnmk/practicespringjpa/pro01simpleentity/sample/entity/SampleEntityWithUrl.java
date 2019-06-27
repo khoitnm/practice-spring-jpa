43f3132a09397e51c5cbe8a8cd0edc1e91bf9363
Kevin Tran <kevin.tran@leonardo.com>
2019-06-27 15:56:31 -0400

    Add some description to SimpleEntity

--- pro01-simple-entity/src/main/java/org/tnmk/practicespringjpa/pro01simpleentity/sample/entity/SampleEntityWithUrl.java
+++ pro01-simple-entity/src/main/java/org/tnmk/practicespringjpa/pro01simpleentity/sample/entity/SampleEntityWithUrl.java
@@ -2,6 +2,12 @@ package org.tnmk.practicespringjpa.pro01simpleentity.sample.entity;
 
 import org.tnmk.practicespringjpa.pro01simpleentity.sample.repository.SampleJdbcRepostiory;
 
+/**
+ * This class show an example that we can compose many columns into one field.
+ * <br/>
+ * The {@link #url} is not an actual column in DB.<br/>
+ * It's actually composited from any different data in DB.
+ */
 public class SampleEntityWithUrl extends SampleEntity {
     /**
      * The select column alias 'url' in query {@link SampleJdbcRepostiory} must match with the {@link #url} field name. You cannot use {@link javax.persistence.Column} here.
--- pro01-simple-entity/src/test/groovy/org/tnmk/common/jpa/columnconverter/json/SampleJdbcTemplateSpec.groovy
+++ pro01-simple-entity/src/test/groovy/org/tnmk/common/jpa/columnconverter/json/SampleJdbcTemplateSpec.groovy
@@ -1,16 +1,11 @@
 package org.tnmk.common.jpa.columnconverter.json
 
-
 import org.springframework.beans.factory.annotation.Autowired
 import org.tnmk.practicespringjpa.pro01simpleentity.sample.entity.SampleEntity
 import org.tnmk.practicespringjpa.pro01simpleentity.sample.entity.SampleEntityWithUrl
-import org.tnmk.practicespringjpa.pro01simpleentity.sample.repository.SampleRepository
 import org.tnmk.practicespringjpa.pro01simpleentity.sample.story.SampleStory
 
 class SampleJdbcTemplateSpec extends BaseSpecification{
-
-    @Autowired
-    SampleRepository sampleRepository;
 
     @Autowired
     SampleStory sampleStory;
@@ -20,7 +15,7 @@ class SampleJdbcTemplateSpec extends BaseSpecification{
         SampleEntity sampleEntity = new SampleEntity(
                 name: "Entity_"+System.nanoTime()
         );
-        sampleRepository.save(sampleEntity);
+        sampleStory.create(sampleEntity);
 
         when:
         List<SampleEntityWithUrl> sampleEntityWithUrlList = sampleStory.findSampleEntitiesWithUrl();
