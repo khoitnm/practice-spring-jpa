3d2ee475b3db0d86098ab183248ba90ba6c196b5
Khoi Tran <khoi.tnm@gmail.com>
2019-06-28 15:29:45 -0400

    Refactor ComparisionTest

--- pro03-json-column/src/test/java/org/tnmk/common/comparision/ComparisionTest.java
+++ pro03-json-column/src/test/java/org/tnmk/common/comparision/ComparisionTest.java
@@ -7,22 +7,23 @@ public class ComparisionTest {
 
 
     @Test
-    public void test() {
-        ChildComparisionEntity childA = new ChildComparisionEntity();
-        childA.setFieldA("a");
-        childA.setFieldB("b"+System.nanoTime());
+    public void test_reflectionEquals_will_use_our_equals_implementation_instead_of_blindly_compare_all_fields() {
+        ChildEntity childA = new ChildEntity();
+        childA.setFirstName("a");
+        childA.setLastName("b"+System.nanoTime());
 
-        ChildComparisionEntity childB = new ChildComparisionEntity();
-        childB.setFieldA(childA.getFieldA());
-        childB.setFieldB("b"+System.nanoTime());
+        ChildEntity childB = new ChildEntity();
+        childB.setFirstName(childA.getFirstName());
+        childB.setLastName("b"+System.nanoTime());
+        /** The last name is different, but it will be ignore because {@link ChildEntity#equals(Object)} ignored it.*/
 
         ParentEntity parentA = new ParentEntity();
         parentA.setName("aaa");
-        parentA.setChildComparisionEntity(childA);
+        parentA.setChildEntity(childA);
 
         ParentEntity parentB = new ParentEntity();
         parentB.setName(parentA.getName());
-        parentB.setChildComparisionEntity(childB);
+        parentB.setChildEntity(childB);
 
         Assert.assertEquals(parentA, parentB);
     }
