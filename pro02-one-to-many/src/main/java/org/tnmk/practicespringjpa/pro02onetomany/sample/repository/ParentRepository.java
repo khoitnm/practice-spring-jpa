c017e45af5f8610d2a39a82ab2f91e236998f12e
Khoi Tran <khoi.tnm@gmail.com>
2019-09-23 09:45:06 -0400

    Change Groovy Spock test to JUnit 5 test so that we can use manually test.

--- pro02-one-to-many/src/main/java/org/tnmk/practicespringjpa/pro02onetomany/sample/repository/ParentRepository.java
+++ pro02-one-to-many/src/main/java/org/tnmk/practicespringjpa/pro02onetomany/sample/repository/ParentRepository.java
@@ -1,9 +1,20 @@
 package org.tnmk.practicespringjpa.pro02onetomany.sample.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.tnmk.practicespringjpa.pro02onetomany.sample.entity.ParentEntity;
+
+import java.util.List;
 
 public interface ParentRepository extends JpaRepository<ParentEntity, Long> {
 
-    ParentEntity findParentAndChildrenByParentId(Long parentId);
+    @Query(value = "SELECT parentEntity FROM ParentEntity parentEntity JOIN FETCH parentEntity.children WHERE parentEntity.parentId = :parentId")
+    ParentEntity findParentAndChildrenByParentId(@Param("parentId") Long parentId);
+
+    @Query(value = "SELECT parentEntity FROM ParentEntity parentEntity JOIN FETCH parentEntity.children WHERE parentEntity.name LIKE :parentName")
+    List<ParentEntity> findParentsAndChildrenByName(@Param("parentName") String parentName);
+
+    @Query(value = "SELECT parentEntity FROM ParentEntity parentEntity JOIN FETCH parentEntity.children")
+    List<ParentEntity> findAllParentsAndChildren();
 }
