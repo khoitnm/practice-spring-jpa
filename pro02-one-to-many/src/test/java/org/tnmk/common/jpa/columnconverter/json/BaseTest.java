8e20b7da95e177608ec903d51d27672ccf501baa
Kevin Tran <kevin.tran@leonardo.com>
2019-09-23 09:55:05 -0400

    Connect real MySQL

--- pro02-one-to-many/src/test/java/org/tnmk/common/jpa/columnconverter/json/BaseTest.java
+++ pro02-one-to-many/src/test/java/org/tnmk/common/jpa/columnconverter/json/BaseTest.java
@@ -1,13 +1,16 @@
 package org.tnmk.common.jpa.columnconverter.json;
 
 import org.junit.Ignore;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.tnmk.practicespringjpa.pro02onetomany.Application;
 
 @Ignore
-@DirtiesContext
+//@DirtiesContext
 @SpringBootTest(classes = Application.class)
+@RunWith(SpringRunner.class)
 public abstract class BaseTest {
 
 }
