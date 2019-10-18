18f81c46b3ccf89575f267dedd66d1431278bd88
Kevin Tran <kevin.tran@leonardo.com>
2019-10-18 17:33:31 -0400

    Add TestContainer

--- pro03-json-column/src/test/java/org/tnmk/practicespringjpa/BaseSpringTest.java
+++ pro03-json-column/src/test/java/org/tnmk/practicespringjpa/BaseSpringTest.java
@@ -6,12 +6,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.tnmk.practicespringjpa.common.embeddeddb.EmbeddedDBContextInitializer;
+import org.tnmk.practicespringjpa.common.testcontainer.DBContainerContextInitializer;
 
 @ActiveProfiles("test")
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {JsonColumnApplication.class})
-@ContextConfiguration(initializers = EmbeddedDBContextInitializer.class)
+@ContextConfiguration(initializers = DBContainerContextInitializer.class)
 @Ignore
 public abstract class BaseSpringTest {
 }
