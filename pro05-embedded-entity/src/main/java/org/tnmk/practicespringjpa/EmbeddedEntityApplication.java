d21120ea3aefea330e6727bd55ccfe9b7f641248
Kevin Tran <kevin.tran@leonardo.com>
2019-12-04 12:31:53 -0500

    Update embedded entities cases: add missing fields.

---
+++ pro05-embedded-entity/src/main/java/org/tnmk/practicespringjpa/EmbeddedEntityApplication.java
@@ -0,0 +1,13 @@
+package org.tnmk.practicespringjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class EmbeddedEntityApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EmbeddedEntityApplication.class, args);
+  }
+}
---
+++ pro05-embedded-entity/src/main/java/org/tnmk/practicespringjpa/correctimplementation/entity/columnconverter/PersonEventConverter.java
@@ -0,0 +1,7 @@
+package org.tnmk.practicespringjpa.correctimplementation.entity.columnconverter;
+
+import org.tnmk.common.jpa.columnconverter.json.JsonConverter;
+import org.tnmk.practicespringjpa.correctimplementation.entity.PersonEvent;
+
+public class PersonEventConverter extends JsonConverter<PersonEvent> {
+}
