bdc2cdeb1551bac8616ea71196384a5531da3262
Khoi Tran <khoi.tnm@gmail.com>
2020-12-22 09:45:46 -0500

    Clone to pro05-partial-update module.

---
+++ pro05-partial-update/src/main/java/org/tnmk/practicespringjpa/pro05partialupdate/Pro05PartialUpdateApplication.java
@@ -0,0 +1,13 @@
+package org.tnmk.practicespringjpa.pro05partialupdate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Pro05PartialUpdateApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Pro05PartialUpdateApplication.class, args);
+  }
+}
