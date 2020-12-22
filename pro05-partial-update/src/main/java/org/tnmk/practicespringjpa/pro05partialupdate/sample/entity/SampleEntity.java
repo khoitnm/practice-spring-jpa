6bbe190f78fce8e626491625cf1b5652fa9b102c
trank <Kevin.Tran@pointclickcare.com>
2020-12-22 09:45:46 -0500

    Clone to pro05-partial-update module.

---
+++ pro05-partial-update/src/main/java/org/tnmk/practicespringjpa/pro05partialupdate/sample/entity/SampleEntity.java
@@ -0,0 +1,42 @@
+package org.tnmk.practicespringjpa.pro05partialupdate.sample.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "sample_entity", catalog = "sample_db")
+public class SampleEntity {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    @Column(name = "sample_entity_id")
+    private Long sampleEntityId;
+
+    private String name;
+
+    @OneToOne
+    @JoinColumn(name = "another_entity_id")
+    private AnotherEntity anotherEntity;
+
+    public Long getSampleEntityId() {
+        return sampleEntityId;
+    }
+
+    public void setSampleEntityId(Long sampleEntityId) {
+        this.sampleEntityId = sampleEntityId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public AnotherEntity getAnotherEntity() {
+        return anotherEntity;
+    }
+
+    public void setAnotherEntity(AnotherEntity anotherEntity) {
+        this.anotherEntity = anotherEntity;
+    }
+}
---
+++ pro05-partial-update/src/main/java/org/tnmk/practicespringjpa/pro05partialupdate/sample/story/SampleStory.java
@@ -0,0 +1,36 @@
+package org.tnmk.practicespringjpa.pro05partialupdate.sample.story;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.tnmk.practicespringjpa.pro05partialupdate.sample.entity.SampleEntity;
+import org.tnmk.practicespringjpa.pro05partialupdate.sample.entity.SampleEntityWithUrl;
+import org.tnmk.practicespringjpa.pro05partialupdate.sample.repository.SampleJdbcRepostiory;
+import org.tnmk.practicespringjpa.pro05partialupdate.sample.repository.SampleRepository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+
+@Transactional // Without this, the service cannot commit transaction, hence cannot create/update/delete items
+@Service
+public class SampleStory {
+    @Autowired
+    private SampleRepository sampleRepository;
+
+    @Autowired
+    private SampleJdbcRepostiory sampleJdbcRepostiory;
+
+    public SampleEntity createSample() {
+        SampleEntity sampleEntity = new SampleEntity();
+        sampleEntity.setName("AAA");
+        return sampleRepository.save(sampleEntity);
+    }
+
+    public SampleEntity create(SampleEntity sampleEntity) {
+        return sampleRepository.save(sampleEntity);
+    }
+
+    public List<SampleEntityWithUrl> findSampleEntitiesWithUrl() {
+        return sampleJdbcRepostiory.findSampleEntitiesWithUrl();
+    }
+}
