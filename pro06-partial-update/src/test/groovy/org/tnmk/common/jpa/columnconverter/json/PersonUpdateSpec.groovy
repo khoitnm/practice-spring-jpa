453cd85af92b626827238f1e920bf7b06cdb7f49
Khoi Tran <khoi.tnm@gmail.com>
2020-12-22 14:00:56 -0500

    Update partial for Person

---
+++ pro06-partial-update/src/test/groovy/org/tnmk/common/jpa/columnconverter/json/PersonUpdateSpec.groovy
@@ -0,0 +1,33 @@
+package org.tnmk.common.jpa.columnconverter.json
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.tnmk.practicespringjpa.pro06partialupdate.sample.entity.PersonEntity
+import org.tnmk.practicespringjpa.pro06partialupdate.sample.model.PersonProfileUpdate
+import org.tnmk.practicespringjpa.pro06partialupdate.sample.story.PersonStory
+
+class PersonUpdateSpec extends BaseSpecification {
+
+    @Autowired
+    PersonStory sampleStory;
+
+    def 'can partial update person profile'() {
+        given:
+        PersonEntity originalPerson = new PersonEntity(
+                fullName: "OriginalName_" + System.nanoTime()
+        );
+        sampleStory.create(originalPerson);
+
+        when:
+        PersonProfileUpdate personProfileUpdate = new PersonProfileUpdate(id: originalPerson.getId(), fullName: "NewName_"+System.nanoTime());
+        int updatedCount = sampleStory.updateProfile(personProfileUpdate);
+
+        List<PersonEntity> foundUpdatedPersons = sampleStory.findPersons();
+        PersonEntity foundUpdatedPerson = foundUpdatedPersons.get(0);
+
+        then:
+        updatedCount == 1
+        foundUpdatedPerson.fullName == personProfileUpdate.fullName
+        foundUpdatedPerson.fullName != originalPerson.fullName
+        foundUpdatedPerson.updateDateTime != originalPerson.updateDateTime
+    }
+}
