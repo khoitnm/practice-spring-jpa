6c31a470e74dc30d3334616225db6770ff5df103
Khoi Tran <khoi.tnm@gmail.com>
2021-03-25 09:55:38 -0400

    Fix: + wrong DB connection URL in DBContainerContextInitializer.java. + wrong catalog name in SampleEntity + wrong DB URL in application.yml

--- pro07-multi-tenant/src/test/java/org/tnmk/practicespringjpa/pro07multitenant/testinfra/dbtestcontainer/DBContainerContextInitializer.java
+++ pro07-multi-tenant/src/test/java/org/tnmk/practicespringjpa/pro07multitenant/testinfra/dbtestcontainer/DBContainerContextInitializer.java
@@ -10,22 +10,19 @@ import java.lang.invoke.MethodHandles;
 
 import static org.tnmk.practicespringjpa.pro07multitenant.testinfra.dbtestcontainer.DBContainerStarter.DB_CONTAINER;
 
-
 public class DBContainerContextInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
 
-    private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+  private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
-    public void initialize(final ConfigurableApplicationContext configurableApplicationContext) {
-        logger.info("DBContainerTest"
-            + "\nJdbcUrl:\t" + DB_CONTAINER.getJdbcUrl()
-            + "\nusername:\t" + DB_CONTAINER.getUsername()
-            + "\npassword:\t" + DB_CONTAINER.getPassword());
-        TestPropertyValues.of(
-            "spring.datasource.url=" + DB_CONTAINER.getJdbcUrl() + "?useSSL=false",
-            "spring.datasource.username=" + DB_CONTAINER.getUsername(),
-            "spring.datasource.password=" + DB_CONTAINER.getPassword(),
-            "spring.datasource.schema-username=" + DB_CONTAINER.getUsername(),
-            "spring.datasource.schema-password=" + DB_CONTAINER.getPassword()
-        ).applyTo(configurableApplicationContext.getEnvironment());
-    }
+  public void initialize(final ConfigurableApplicationContext configurableApplicationContext) {
+    logger.info("DBContainerTest"
+        + "\nJdbcUrl:\t" + DB_CONTAINER.getJdbcUrl()
+        + "\nusername:\t" + DB_CONTAINER.getUsername()
+        + "\npassword:\t" + DB_CONTAINER.getPassword());
+    TestPropertyValues.of(
+        "spring.datasource.url=" + DB_CONTAINER.getJdbcUrl(),
+        "spring.datasource.username=" + DB_CONTAINER.getUsername(),
+        "spring.datasource.password=" + DB_CONTAINER.getPassword()
+    ).applyTo(configurableApplicationContext.getEnvironment());
+  }
 }
