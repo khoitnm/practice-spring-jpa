bf1b228d09523c34f05cc05ba4e82919f329ea00
trank <Kevin.Tran@pointclickcare.com>
2021-03-25 05:50:26 -0400

    Update Flyway configuration: enable validate-on-migrate

---
+++ pro04-flyway/src/test/groovy/org/tnmk/common/jpa/columnconverter/json/FlywayTest.groovy
@@ -0,0 +1,21 @@
+package org.tnmk.common.jpa.columnconverter.json
+
+import org.flywaydb.core.Flyway
+import org.springframework.beans.factory.annotation.Autowired
+
+class FlywayTest extends BaseSpecification {
+
+  @Autowired
+  Flyway flyway;
+
+  def 'Can run flyway successfully'(){
+    given:
+    flyway.clean();
+
+    when:
+    flyway.migrate();
+
+    then:
+    noExceptionThrown();
+  }
+}
