859b1428ba82348121422b30593d8d395e0bdf66
trank <Kevin.Tran@pointclickcare.com>
2021-10-26 20:11:04 -0400

    Start crateSample when running the app.

--- pro01-simple-entity/src/main/java/org/tnmk/practicespringjpa/pro01simpleentity/sample/story/SampleStory.java
+++ pro01-simple-entity/src/main/java/org/tnmk/practicespringjpa/pro01simpleentity/sample/story/SampleStory.java
@@ -1,6 +1,10 @@
 package org.tnmk.practicespringjpa.pro01simpleentity.sample.story;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.tnmk.practicespringjpa.pro01simpleentity.sample.entity.SampleEntity;
 import org.tnmk.practicespringjpa.pro01simpleentity.sample.entity.SampleEntityWithUrl;
@@ -10,27 +14,32 @@ import org.tnmk.practicespringjpa.pro01simpleentity.sample.repository.SampleRepo
 import javax.transaction.Transactional;
 import java.util.List;
 
-
 @Transactional // Without this, the service cannot commit transaction, hence cannot create/update/delete items
 @Service
 public class SampleStory {
-    @Autowired
-    private SampleRepository sampleRepository;
+  @Autowired
+  private SampleRepository sampleRepository;
 
-    @Autowired
-    private SampleJdbcRepostiory sampleJdbcRepostiory;
+  @Autowired
+  private SampleJdbcRepostiory sampleJdbcRepostiory;
 
-    public SampleEntity createSample() {
-        SampleEntity sampleEntity = new SampleEntity();
-        sampleEntity.setName("AAA");
-        return sampleRepository.save(sampleEntity);
-    }
+  @EventListener(ApplicationStartedEvent.class)
+  public SampleEntity createSample() {
+    SampleEntity sampleEntity = new SampleEntity();
+    sampleEntity.setName("AAA");
+    sampleEntity = sampleRepository.save(sampleEntity);
 
-    public SampleEntity create(SampleEntity sampleEntity) {
-        return sampleRepository.save(sampleEntity);
-    }
+    findSampleEntitiesWithUrl();
 
-    public List<SampleEntityWithUrl> findSampleEntitiesWithUrl() {
-        return sampleJdbcRepostiory.findSampleEntitiesWithUrl();
-    }
+    return sampleEntity;
+  }
+
+  public SampleEntity create(SampleEntity sampleEntity) {
+    return sampleRepository.save(sampleEntity);
+  }
+
+  public List<SampleEntityWithUrl> findSampleEntitiesWithUrl() {
+    sampleRepository.findAll(PageRequest.of(0, 10));
+    return sampleJdbcRepostiory.findSampleEntitiesWithUrl();
+  }
 }
