74a1e5c2c861c27eb5df5230dbe927cfd303c108
Khoi Tran <khoi.tnm@gmail.com>
2022-03-30 20:20:31 -0400

    Test case about UnexpectedRollbackException: MainService_CatchAllExceptions_Test.

--- pro10-transaction-simple/src/test/java/org/tnmk/practicespringjpa/pro10transactionsimple/practice_01_mainlogic_catch_exception_in_nested_tnx/MainService_CatchAllExceptions_Test.java
+++ pro10-transaction-simple/src/test/java/org/tnmk/practicespringjpa/pro10transactionsimple/practice_01_mainlogic_catch_exception_in_nested_tnx/MainService_CatchAllExceptions_Test.java
@@ -2,16 +2,20 @@ package org.tnmk.practicespringjpa.pro10transactionsimple.practice_01_mainlogic_
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.UnexpectedRollbackException;
 import org.tnmk.practicespringjpa.pro10transactionsimple.common.SimpleEntity;
 import org.tnmk.practicespringjpa.pro10transactionsimple.common.SimpleRepository;
 import org.tnmk.practicespringjpa.pro10transactionsimple.practice_00_simple_nested_tnx.SaveEntitiesResult;
 import org.tnmk.practicespringjpa.pro10transactionsimple.testinfra.BaseSpringTest_WithActualDb;
 
 import javax.transaction.Transactional;
+import java.util.Collections;
 import java.util.Optional;
+import java.util.UUID;
 
 public class MainService_CatchAllExceptions_Test extends BaseSpringTest_WithActualDb {
   @Autowired
@@ -29,7 +33,6 @@ public class MainService_CatchAllExceptions_Test extends BaseSpringTest_WithActu
   @ParameterizedTest
   @CsvSource(value = {
       //entityInMainMethod  ,entityInPrivateMethod ,entityInNestedService ,expectSavedInMainMethod, expectSavedInPrivateMethod, expectedSavedInNestedMethod
-      "Name01               ,Name02                ,                      ,true                   , true                      , false",
       "Name01               ,                      ,Name03                ,true                   , false                     , true",
       "                     ,Name02                ,Name03                ,false                  , true                      , true"
   })
@@ -45,7 +48,7 @@ public class MainService_CatchAllExceptions_Test extends BaseSpringTest_WithActu
     // Given
     SimpleEntity toBeSavedInMainMethod = new SimpleEntity(entityInMainMethod);
     SimpleEntity toBeSavedInPrivateMethod = new SimpleEntity(entityInPrivateMethod);
-    SimpleEntity toBeSavedInNestedService = new SimpleEntity(entityInNestedService); // this will cause error
+    SimpleEntity toBeSavedInNestedService = new SimpleEntity(entityInNestedService);
 
     // When
     SaveEntitiesResult result = mainService.saveEntities(toBeSavedInMainMethod, toBeSavedInPrivateMethod, toBeSavedInNestedService);
@@ -58,6 +61,25 @@ public class MainService_CatchAllExceptions_Test extends BaseSpringTest_WithActu
     }
     if (expectedSavedInNestedMethod) {
       assertExist(result.getToBeSavedInNestedService(), true);
+    }
+  }
+
+  @Test
+  public void when_get_UnexpectedRollbackException__then__all_data_including_mainService_will_be_rolled_back() {
+    // Given
+    SimpleEntity toBeSavedInMainMethod = new SimpleEntity("Name" + UUID.randomUUID());
+    SimpleEntity toBeSavedInPrivateMethod = new SimpleEntity("Name" + UUID.randomUUID());
+
+    // this will cause UnexpectedRollbackException because the transaction in NestedBusiness will be rolled back,
+    // but the transaction in MainBusiness won't aware of it (because of catching).
+    // so UnexpectedRollbackException will be thrown.
+    SimpleEntity toBeSavedInNestedService = new SimpleEntity(null);
+
+    // When
+    try {
+      mainService.saveEntities(toBeSavedInMainMethod, toBeSavedInPrivateMethod, toBeSavedInNestedService);
+    } catch (UnexpectedRollbackException ex) {
+      Assertions.assertEquals(Collections.emptyList(), simpleRepository.findAll());
     }
   }
 
