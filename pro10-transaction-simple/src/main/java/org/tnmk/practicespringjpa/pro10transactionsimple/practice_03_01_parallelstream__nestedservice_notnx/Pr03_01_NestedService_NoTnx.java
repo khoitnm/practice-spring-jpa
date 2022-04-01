8f86c95406b7c852caaf10ff172a6e97674bb498
Khoi Tran <khoi.tnm@gmail.com>
2022-04-01 07:45:30 -0400

    Add Pr03 for parallel test.

---
+++ pro10-transaction-simple/src/main/java/org/tnmk/practicespringjpa/pro10transactionsimple/practice_03_01_parallelstream__nestedservice_notnx/Pr03_01_NestedService_NoTnx.java
@@ -0,0 +1,23 @@
+package org.tnmk.practicespringjpa.pro10transactionsimple.practice_03_01_parallelstream__nestedservice_notnx;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.tnmk.practicespringjpa.pro10transactionsimple.common.SimpleEntity;
+import org.tnmk.practicespringjpa.pro10transactionsimple.common.SimpleRepository;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class Pr03_01_NestedService_NoTnx {
+  private final SimpleRepository simpleRepository;
+
+  public void save(String entityName) throws IllegalArgumentException {
+    log.info("Saving '{}': starting ...", entityName);
+    if (entityName == null) {
+      throw new IllegalArgumentException("name cannot be null");
+    }
+    SimpleEntity result = simpleRepository.save(new SimpleEntity(entityName));
+    log.info("Saving '{}': finished!", entityName);
+  }
+}
