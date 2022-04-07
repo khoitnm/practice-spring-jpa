2e7f91bc6a43470405814ed1066b02e29506be41
Khoi Tran <khoi.tnm@gmail.com>
2022-04-07 11:18:14 -0400

    Show example of delete by name.

--- pro01-simple-entity/src/main/java/org/tnmk/practicespringjpa/pro01simpleentity/sample/repository/SampleRepository.java
+++ pro01-simple-entity/src/main/java/org/tnmk/practicespringjpa/pro01simpleentity/sample/repository/SampleRepository.java
@@ -1,8 +1,13 @@
 package org.tnmk.practicespringjpa.pro01simpleentity.sample.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.tnmk.practicespringjpa.pro01simpleentity.sample.entity.SampleEntity;
 
+import javax.transaction.Transactional;
+
 public interface SampleRepository extends JpaRepository<SampleEntity, Long> {
+  @Modifying
+  @Transactional
+  void deleteByName(String name);
 }
