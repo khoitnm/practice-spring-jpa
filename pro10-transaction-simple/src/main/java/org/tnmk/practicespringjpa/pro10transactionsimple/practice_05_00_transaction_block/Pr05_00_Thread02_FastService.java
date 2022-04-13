ae77d6a1617b7104283cba343d8a8852af9bc07d
trank <Kevin.Tran@pointclickcare.com>
2022-04-12 22:15:41 -0400

    Fix timezone problem in pro10

--- pro10-transaction-simple/src/main/java/org/tnmk/practicespringjpa/pro10transactionsimple/practice_05_00_transaction_block/Pr05_00_Thread02_FastService.java
+++ pro10-transaction-simple/src/main/java/org/tnmk/practicespringjpa/pro10transactionsimple/practice_05_00_transaction_block/Pr05_00_Thread02_FastService.java
@@ -4,12 +4,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
 import org.tnmk.practicespringjpa.pro10transactionsimple.common.SimpleEntity;
 import org.tnmk.practicespringjpa.pro10transactionsimple.common.SimpleRepository;
 
-import javax.transaction.Transactional;
 import java.time.Duration;
-import java.time.ZonedDateTime;
 import java.time.ZonedDateTime;
 import java.util.concurrent.CompletableFuture;
 
@@ -21,7 +21,7 @@ public class Pr05_00_Thread02_FastService {
   private final SimpleRepository simpleRepository;
 
   @Async
-  @Transactional
+  @Transactional(isolation = Isolation.SERIALIZABLE)
   public CompletableFuture<ZonedDateTime> async_createEntity_fast(String name) {
     ZonedDateTime start = ZonedDateTime.now();
 
