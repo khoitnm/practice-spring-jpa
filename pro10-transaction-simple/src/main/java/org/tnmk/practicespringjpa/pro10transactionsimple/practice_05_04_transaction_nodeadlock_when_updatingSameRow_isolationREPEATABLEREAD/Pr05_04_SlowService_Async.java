4cc3fb8254765acc88839de7089e71a836d8b3d4
Khoi Tran <khoi.tnm@gmail.com>
2022-04-13 23:41:35 -0400

    + practice_05_03_transaction_nodeadlock_when_savingSameEntity_with_isolationREPEATREAD: have a clear explanation. + Add practice_05_04_transaction_nodeadlock_when_updatingSameRow_with_isolationREPEATREAD

--- pro10-transaction-simple/src/main/java/org/tnmk/practicespringjpa/pro10transactionsimple/practice_05_04_transaction_nodeadlock_when_updatingSameRow_isolationREPEATABLEREAD/Pr05_04_SlowService_Async.java
+++ pro10-transaction-simple/src/main/java/org/tnmk/practicespringjpa/pro10transactionsimple/practice_05_04_transaction_nodeadlock_when_updatingSameRow_isolationREPEATABLEREAD/Pr05_04_SlowService_Async.java
@@ -1,4 +1,4 @@
-package org.tnmk.practicespringjpa.pro10transactionsimple.practice_05_03_transaction_nodeadlock_when_savingSameEntity_with_isolationREPEATREAD;
+package org.tnmk.practicespringjpa.pro10transactionsimple.practice_05_04_transaction_nodeadlock_when_updatingSameRow_isolationREPEATABLEREAD;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -6,8 +6,6 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.support.TransactionSynchronizationManager;
-import org.tnmk.practicespringjpa.pro10transactionsimple.common.SimpleEntity;
 import org.tnmk.practicespringjpa.pro10transactionsimple.common.SimpleRepository;
 import org.tnmk.practicespringjpa.pro10transactionsimple.common.utils.ThreadUtils;
 
@@ -18,26 +16,25 @@ import java.util.concurrent.CompletableFuture;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class Pr05_03_SlowService_Async {
+public class Pr05_04_SlowService_Async {
 
   private final SimpleRepository simpleRepository;
 
   @Async
   @Transactional(isolation = Isolation.REPEATABLE_READ)
-  public CompletableFuture<ZonedDateTime> async_editEntity_Slow(SimpleEntity simpleEntity, int delayMillis) {
-    log.info("edit slow entity: start " + TransactionSynchronizationManager.getCurrentTransactionName());
+  public CompletableFuture<ZonedDateTime> async_editEntity_Slow(String newName, long entityId, int delayMillis) {
+    log.info("edit slow entity: start");
 
     ZonedDateTime start = ZonedDateTime.now();
 
-    simpleEntity = simpleRepository.save(simpleEntity);
+    simpleRepository.updateNameById(newName, entityId);
     log.info("edit slow entity: saved");
 
     ThreadUtils.sleep(delayMillis);
 
     ZonedDateTime end = ZonedDateTime.now();
     Duration duration = Duration.between(start, end);
-    log.info("edit slow entity: finished in {} with transaction {}", (double) duration.toMillis() / 1000d,
-        TransactionSynchronizationManager.getCurrentTransactionName());
+    log.info("edit slow entity: finished in {}s", (double) duration.toMillis() / 1000d);
     return CompletableFuture.completedFuture(end);
   }
 }
