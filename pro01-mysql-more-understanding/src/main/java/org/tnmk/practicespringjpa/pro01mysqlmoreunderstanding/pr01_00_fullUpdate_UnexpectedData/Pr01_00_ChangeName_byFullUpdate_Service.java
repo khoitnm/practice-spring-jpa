baeb591586f77dfe25c7ca2c74d43a8ca7d538de
Khoi Tran <khoi.tnm@gmail.com>
2022-04-18 12:39:05 -0400

    pro01-mysql-more-understanding: sample code to illustrate fullUpdate problem.

---
+++ pro01-mysql-more-understanding/src/main/java/org/tnmk/practicespringjpa/pro01mysqlmoreunderstanding/pr01_00_fullUpdate_UnexpectedData/Pr01_00_ChangeName_byFullUpdate_Service.java
@@ -0,0 +1,25 @@
+package org.tnmk.practicespringjpa.pro01mysqlmoreunderstanding.pr01_00_fullUpdate_UnexpectedData;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.tnmk.practicespringjpa.pro01mysqlmoreunderstanding.common.SimpleEntity;
+import org.tnmk.practicespringjpa.pro01mysqlmoreunderstanding.common.SimpleRepository;
+
+import java.util.concurrent.CompletableFuture;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class Pr01_00_ChangeName_byFullUpdate_Service {
+    private final SimpleRepository simpleRepository;
+
+    @Async
+    public CompletableFuture<Void> async_NoTnx_ChangeName(long entityId, String updateName) {
+        SimpleEntity simpleEntity = simpleRepository.findById(entityId).get();
+        simpleEntity.setName(updateName);
+        simpleRepository.save(simpleEntity);
+        return CompletableFuture.completedFuture(null);
+    }
+}
