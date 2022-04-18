e2559b4082ccd9aae4b9c62ab190c8b9c79ff236
Khoi Tran <khoi.tnm@gmail.com>
2022-04-17 22:01:16 -0400

    Add a case to illustrate that Spring JPA only update entity at the end of transaction. It means it will cause unexpected data in Pr05_05_ExternalSystemSimulator.

--- pro10-transaction-simple/src/main/java/org/tnmk/practicespringjpa/pro10transactionsimple/common/SimpleRepository.java
+++ pro10-transaction-simple/src/main/java/org/tnmk/practicespringjpa/pro10transactionsimple/common/SimpleRepository.java
@@ -4,13 +4,19 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface SimpleRepository extends JpaRepository<SimpleEntity, Long> {
-  Optional<SimpleEntity> findByName(String entityName);
+    Optional<SimpleEntity> findByName(String entityName);
 
-  @Query("UPDATE SimpleEntity e SET e.name = :name WHERE e.id = :id")
-  @Modifying
-  void updateNameById(@Param("name") String newName,@Param("id") Long id);
+    @Query("UPDATE SimpleEntity e SET e.name = :name WHERE e.id = :id")
+    @Modifying
+    void updateNameById(@Param("name") String newName, @Param("id") Long id);
+
+    @Query(value = "INSERT INTO SimpleEntity(name) VALUES (:name)", nativeQuery = true)
+    @Modifying
+    void insertName(@Param("name") String newName);
 }
---
+++ pro10-transaction-simple/src/test/java/org/tnmk/practicespringjpa/pro10transactionsimple/practice_05_05_transaction_nodeadlock_when_saveEntityRollbackProblem/Pr05_05_MainService_Test.java
@@ -0,0 +1,45 @@
+package org.tnmk.practicespringjpa.pro10transactionsimple.practice_05_05_transaction_nodeadlock_when_saveEntityRollbackProblem;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.InvalidDataAccessResourceUsageException;
+import org.tnmk.practicespringjpa.pro10transactionsimple.common.SimpleRepository;
+import org.tnmk.practicespringjpa.pro10transactionsimple.testinfra.BaseSpringTest_WithActualDb;
+
+import java.time.ZonedDateTime;
+import java.util.TimeZone;
+import java.util.UUID;
+import java.util.concurrent.CompletionException;
+import java.util.concurrent.ExecutionException;
+
+@Slf4j
+public class Pr05_05_MainService_Test extends BaseSpringTest_WithActualDb {
+    @Autowired
+    private Pr05_05_MainService service;
+
+    @Autowired
+    private SimpleRepository simpleRepository;
+    @Autowired
+    private Pr05_05_ExternalSystemSimulator externalSystemSimulator;
+
+    @Test
+    public void test_startSlowBeforeFast() throws ExecutionException, InterruptedException {
+        ZonedDateTime startTime = ZonedDateTime.now();
+
+        log.info("Timezone: " + TimeZone.getDefault());
+
+        String insertedName = "ToBeUpdatedName" + UUID.randomUUID();
+        int serviceRuntime = 2000;
+        int delayBetweenServices = 1000;
+
+
+        try {
+            Pr05_05_MainService.Result result = service.editFirst_insertDuplicateLater(insertedName, serviceRuntime, delayBetweenServices);
+        } catch (CompletionException ex) {
+            Assertions.assertTrue(ex.getCause() instanceof InvalidDataAccessResourceUsageException);
+            Assertions.assertTrue(externalSystemSimulator.hasItem(insertedName));
+        }
+    }
+}
