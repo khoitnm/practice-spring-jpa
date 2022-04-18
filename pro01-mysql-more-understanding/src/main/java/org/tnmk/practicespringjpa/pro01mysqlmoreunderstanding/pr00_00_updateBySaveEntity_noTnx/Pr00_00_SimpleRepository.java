849d03d140958c6d61c70d6becd22b6e77fcaa66
Khoi Tran <khoi.tnm@gmail.com>
2022-04-18 11:12:18 -0400

    pro01-mysql-more-understanding: pr00_00

--- pro01-mysql-more-understanding/src/main/java/org/tnmk/practicespringjpa/pro01mysqlmoreunderstanding/pr00_00_updateBySaveEntity_noTnx/Pr00_00_SimpleRepository.java
+++ pro01-mysql-more-understanding/src/main/java/org/tnmk/practicespringjpa/pro01mysqlmoreunderstanding/pr00_00_updateBySaveEntity_noTnx/Pr00_00_SimpleRepository.java
@@ -1,7 +1,10 @@
-package org.tnmk.practicespringjpa.pro01mysqlmoreunderstanding.pr00_00_saveentity_for_updating;
+package org.tnmk.practicespringjpa.pro01mysqlmoreunderstanding.pr00_00_updateBySaveEntity_noTnx;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.tnmk.practicespringjpa.pro01mysqlmoreunderstanding.common.SimpleEntity;
 
+import java.util.Optional;
+
 public interface Pr00_00_SimpleRepository extends JpaRepository<SimpleEntity, Long> {
+    Optional<SimpleEntity> findByName(String name);
 }
