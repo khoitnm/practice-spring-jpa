b74a0fea2fd930c2820322568d4fa1a78eba4c2d
Khoi Tran <khoi.tnm@gmail.com>
2022-05-03 18:57:47 -0400

    Fix some test data package.

--- pro01-mysql-more-understanding/src/test/java/org/tnmk/practicespringjpa/pro01mysqlmoreunderstanding/pr03_eager_vs_lazy_load/pr03_00_eager/testdata/ParentAndChildrenWithEagerLoad.java
+++ pro01-mysql-more-understanding/src/test/java/org/tnmk/practicespringjpa/pro01mysqlmoreunderstanding/pr03_eager_vs_lazy_load/pr03_00_eager/testdata/ParentAndChildrenWithEagerLoad.java
@@ -6,7 +6,6 @@ import org.tnmk.practicespringjpa.pro01mysqlmoreunderstanding.pr03_eager_vs_lazy
 import org.tnmk.practicespringjpa.pro01mysqlmoreunderstanding.pr03_eager_vs_lazy_load.pr03_00_eager.ChildWithEagerLoadEntity;
 
 import java.util.List;
-
 @RequiredArgsConstructor
 @Getter
 public class ParentAndChildrenWithEagerLoad {
