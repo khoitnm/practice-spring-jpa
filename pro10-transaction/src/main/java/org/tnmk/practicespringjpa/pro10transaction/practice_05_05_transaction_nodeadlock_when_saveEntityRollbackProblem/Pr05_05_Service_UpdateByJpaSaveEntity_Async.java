b5312f6e3d2a0561f871b2a9fb3cf26f6e3369e6
Khoi Tran <khoi.tnm@gmail.com>
2022-05-04 23:54:57 -0400

    Simplify Pr05_05

--- pro10-transaction/src/main/java/org/tnmk/practicespringjpa/pro10transaction/practice_05_05_transaction_nodeadlock_when_saveEntityRollbackProblem/Pr05_05_Service_UpdateByJpaSaveEntity_Async.java
+++ pro10-transaction/src/main/java/org/tnmk/practicespringjpa/pro10transaction/practice_05_05_transaction_nodeadlock_when_saveEntityRollbackProblem/Pr05_05_Service_UpdateByJpaSaveEntity_Async.java
@@ -25,27 +25,18 @@ public class Pr05_05_Service_UpdateByJpaSaveEntity_Async {
     @Async
     @Transactional(isolation = Isolation.REPEATABLE_READ)
     public CompletableFuture<ZonedDateTime> async_updateEntity(SimpleEntity simpleEntity, String updateName, int delayMillis) {
-        MDC.put("thread", "jpaSaveEntity");
+        log.info("async_updateEntity: start...");
 
-        log.info("jpaSaveEntity: start");
-
-        ZonedDateTime start = ZonedDateTime.now();
-
+        log.info("save entity: start...");
         simpleEntity.setName(updateName);
         simpleRepository.save(simpleEntity);
-        log.info("jpaSaveEntity: saved");
+        log.info("save entity: finished!");
 
-        log.info("jpaSaveEntity: addEntity to an externalSystem: starting\n" +
-                "\tAnd if the previous jpaSave failed (for whatever reason), no data is saved into the other system.\n" +
-                "\tIf the previous jpaSave succeed, we'll try to save data into the other system." +
-                "\tAnd if we cannot save data into the other system, rollback the data that saved into DB.");
+        log.info("addEntity to externalSystem: starting...");
         externalSystemSimulator.addEntity(simpleEntity, delayMillis);
-        log.info("jpaSaveEntity: addEntity to an externalSystem: finished!!!\n" +
-                "\tIf we see this log, it means we cannot rollback data in the other system anymore.");
+        log.info("addEntity to externalSystem: finished!!!");
 
-        ZonedDateTime end = ZonedDateTime.now();
-        Duration duration = Duration.between(start, end);
-        log.info("jpaSaveEntity: finished in {}s", (double) duration.toMillis() / 1000d);
-        return CompletableFuture.completedFuture(end);
+        log.info("async_updateEntity: finished!!!");
+        return CompletableFuture.completedFuture(ZonedDateTime.now());
     }
 }
