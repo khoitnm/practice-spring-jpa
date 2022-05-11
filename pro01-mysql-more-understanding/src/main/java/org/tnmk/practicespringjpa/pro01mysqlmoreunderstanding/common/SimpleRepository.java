a4f71ced8c3b7e3ec755c56b4b914e4d3ad2f4fa
Khoi Tran <khoi.tnm@gmail.com>
2022-05-10 23:20:53 -0400

    Sample code of pagination.

--- pro01-mysql-more-understanding/src/main/java/org/tnmk/practicespringjpa/pro01mysqlmoreunderstanding/common/SimpleRepository.java
+++ pro01-mysql-more-understanding/src/main/java/org/tnmk/practicespringjpa/pro01mysqlmoreunderstanding/common/SimpleRepository.java
@@ -1,8 +1,14 @@
 package org.tnmk.practicespringjpa.pro01mysqlmoreunderstanding.common;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SimpleRepository extends JpaRepository<SimpleEntity, Long> {
+
+  Page<SimpleEntity> findByNameContaining(String name, Pageable pageable);
 }
---
+++ pro01-mysql-more-understanding/src/test/java/org/tnmk/practicespringjpa/pro01mysqlmoreunderstanding/pro01_04_pagination/Pro01_04_PaginationServiceTest.java
@@ -0,0 +1,41 @@
+package org.tnmk.practicespringjpa.pro01mysqlmoreunderstanding.pro01_04_pagination;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.tnmk.practicespringjpa.pro01mysqlmoreunderstanding.common.SimpleEntity;
+import org.tnmk.practicespringjpa.pro01mysqlmoreunderstanding.common.SimpleRepository;
+import org.tnmk.practicespringjpa.pro01mysqlmoreunderstanding.common.testdata.SimpleEntityFixture;
+import org.tnmk.practicespringjpa.pro01mysqlmoreunderstanding.testinfra.BaseSpringTest_WithActualDb;
+
+import java.util.List;
+
+@Slf4j
+public class Pro01_04_PaginationServiceTest extends BaseSpringTest_WithActualDb {
+  @Autowired
+  private Pro01_04_PaginationService service;
+
+  @Autowired
+  private SimpleEntityFixture simpleEntityFixture;
+  @Autowired
+  private SimpleRepository simpleRepository;
+
+  @Test
+  public void when_doingPagination_then_2QueriesAreExecuted_OneForFindElementsInAPage_AndAnotherForCountingTotalElements() {
+    // Given
+    log.info("Test: Prepare data");
+    int entitiesCount = 5;
+    simpleEntityFixture.random(entitiesCount);
+
+    // When
+    log.info("Test: when executing the main logic");
+    PageRequest pageRequest = PageRequest.of(1, 2);
+    List<SimpleEntity> entities = service.findByNameContaining(SimpleEntityFixture.ENTITY_NAME_PREFIX, pageRequest);
+
+    // Then
+    log.info("Test: then assert the result");
+    Assertions.assertEquals(pageRequest.getPageSize(), entities.size());
+  }
+}
