a6a2aeb7edff17e5cddab06bb50548ccbc8b7ffa
Khoi Tran <khoi.tnm@gmail.com>
2022-06-14 12:54:56 -0400

    Try to change R__insert_sample_entities.sql (not successfully yet)

--- pro04-flyway/src/main/resources/db/migration/R__insert_sample_entities.sql
+++ pro04-flyway/src/main/resources/db/migration/R__insert_sample_entities.sql
@@ -4,8 +4,13 @@ VALUES ('A', 'Code_A');
 INSERT INTO sample_entity (name, entity_code)
 VALUES ('B', 'Code_B');
 
+-- this insert will cause error because of duplicate 'Code_B'
 INSERT INTO sample_entity (name, entity_code)
-VALUES ('Error', 'Code_B'); -- this insert will cause error because of duplicate 'Code_B'
+VALUES ('ErrorB', 'Code_B');
 
 INSERT INTO sample_entity (name, entity_code)
 VALUES ('C', 'Code_C');
+
+-- this insert will cause error because of duplicate 'Code_C'
+INSERT INTO sample_entity (name, entity_code)
+VALUES ('ErrorC', 'Code_C');
