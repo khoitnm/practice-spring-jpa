c38b3edcd582023f041c988c4afbc4e04a21b193
Khoi Tran <khoi.tnm@gmail.com>
2022-06-14 13:40:08 -0400

    Fix R__insert_sample_entities.sql: apply some error record in the script to demonstrate how the error will be reported.

--- pro04-flyway/src/main/java/org/tnmk/practicespringjpa/pro04flyway/FlywayStrategy.java
+++ pro04-flyway/src/main/java/org/tnmk/practicespringjpa/pro04flyway/FlywayStrategy.java
@@ -4,6 +4,7 @@ import org.flywaydb.core.Flyway;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.stereotype.Component;
 
 /**
  * Only enable this when there a fail flyway script was executed.
@@ -11,13 +12,13 @@ import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 //@Component
 public class FlywayStrategy implements FlywayMigrationStrategy {
 
-    private static final Logger logger = LoggerFactory.getLogger(FlywayStrategy.class);
+  private static final Logger logger = LoggerFactory.getLogger(FlywayStrategy.class);
 
-    @Override
-    public void migrate(Flyway flyway) {
-        logger.info("running flyway");
-        flyway.repair();//This code is use to repair all wrong executed flyway script by remove the corresponding record in DB.
-        flyway.migrate();
-        logger.info("done running flyway");
-    }
+  @Override
+  public void migrate(Flyway flyway) {
+    logger.info("running flyway");
+    flyway.repair();//This code is use to repair all wrong executed flyway script by remove the corresponding record in DB.
+    flyway.migrate();
+    logger.info("done running flyway");
+  }
 }
