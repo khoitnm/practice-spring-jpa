b527d5ff0ded7060ade221558ab24f49c5395368
Khoi Tran <khoi.tnm@gmail.com>
2022-06-14 15:43:01 -0400

    Update SampleBatchJdbcRepository to return inserted ids from batch.

--- pro00-mssql-01-jdbc-batch/src/main/java/org/tnmk/practicespringjpa/pro00mssql01jdbcbatch/sample/batch/SampleBatchJdbcRepository.java
+++ pro00-mssql-01-jdbc-batch/src/main/java/org/tnmk/practicespringjpa/pro00mssql01jdbcbatch/sample/batch/SampleBatchJdbcRepository.java
@@ -9,7 +9,9 @@ import java.sql.BatchUpdateException;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -33,31 +35,27 @@ public class SampleBatchJdbcRepository {
         Connection connection = dataSource.getConnection();
         // We can also use Statement.RETURN_GENERATED_KEYS instead of generatedColumns,
         // but it looks like generatedColumns is a safer option (because it doesn't rely on Jdbc Driver).
-        PreparedStatement statement = connection.prepareStatement(sqlInsert, generatedColumns);
+        PreparedStatement statement = connection.prepareStatement(sqlInsert, Statement.RETURN_GENERATED_KEYS);
     ) {
       for (CreateEntityRequest request : requests) {
         statement.setString(1, request.getName());
         statement.setString(2, request.getEntityCode());
         statement.setDate(3, new Date(Instant.now().toEpochMilli()));
         statement.addBatch();
-
-        // Just TMP
-        CreateEntityResult result = new CreateEntityResult(request, 0);// Don't know the id
-        results.add(result);
       }
       int[] insertedRows = statement.executeBatch();
-      log.info("Inserted Rows: " + insertedRows);
+      log.info("Inserted Rows: {}", Arrays.toString(insertedRows));
 
-      //      int iRequest = 0;
-      //      try (ResultSet rs = statement.getGeneratedKeys()) {
-      //        while (rs.next()) {
-      //          CreateEntityRequest request = requests.get(iRequest);
-      //          long id = rs.getLong(1);
-      //          CreateEntityResult result = new CreateEntityResult(request, id);
-      //          results.add(result);
-      //          iRequest++;
-      //        }
-      //      }
+      int iRequest = 0;
+      try (ResultSet rs = statement.getGeneratedKeys()) {
+        while (rs.next()) {
+          CreateEntityRequest request = requests.get(iRequest);
+          long id = rs.getLong(1);
+          CreateEntityResult result = new CreateEntityResult(request, id);
+          results.add(result);
+          iRequest++;
+        }
+      }
       return results;
     } catch (BatchUpdateException ex) {
       log.info("Update counts: {}", Arrays.toString(ex.getUpdateCounts()));
