0a7c6648e4df2e7d065b73e4aaae6091d3f452d6
Khoi Tran <khoi.tnm@gmail.com>
2022-06-14 16:23:05 -0400

    Update comment to explain why we disable SampleBatchJdbcRepositoryTest

--- pro00-mssql-01-jdbc-batch/src/test/java/org/tnmk/practicespringjpa/pro00mssql01jdbcbatch/sample/batch/SampleBatchJdbcRepositoryTest.java
+++ pro00-mssql-01-jdbc-batch/src/test/java/org/tnmk/practicespringjpa/pro00mssql01jdbcbatch/sample/batch/SampleBatchJdbcRepositoryTest.java
@@ -3,16 +3,24 @@ package org.tnmk.practicespringjpa.pro00mssql01jdbcbatch.sample.batch;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.tnmk.practicespringjpa.pro00mssql01jdbcbatch.sample.SampleEntity;
 import org.tnmk.practicespringjpa.pro00mssql01jdbcbatch.sample.SampleRepository;
 import org.tnmk.practicespringjpa.pro00mssql01jdbcbatch.testinfra.BaseSpringTest_WithActual_MsSqlServer;
+import org.tnmk.practicespringjpa.pro00mssql01jdbcbatch.testinfra.BaseSpringTest_WithActual_MySql;
 
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * As mentioned in {@link SampleBatchJdbcRepository_NotWorkFor_MsSqlServer},
+ * This test case can work with MySQL (requires MySQL dependency in pom.xml), but not with MS SQL Server.
+ * So I'm disabling it for now.
+ */
+@Disabled
 @Slf4j
-public class SampleBatchJdbcRepositoryTest extends BaseSpringTest_WithActual_MsSqlServer {
+public class SampleBatchJdbcRepositoryTest extends BaseSpringTest_WithActual_MySql {
   @Autowired
   private SampleBatchJdbcRepository_NotWorkFor_MsSqlServer batchJdbcRepository;
 
