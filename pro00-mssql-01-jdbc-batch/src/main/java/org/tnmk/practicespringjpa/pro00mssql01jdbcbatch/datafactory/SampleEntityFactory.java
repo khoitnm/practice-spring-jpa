69e4ce9e490ad40e598ec5e7153f412292fb5779
Khoi Tran <khoi.tnm@gmail.com>
2022-06-14 14:30:20 -0400

    Add uniqueColumn 'entity_code' into SampleEntity.

--- pro00-mssql-01-jdbc-batch/src/main/java/org/tnmk/practicespringjpa/pro00mssql01jdbcbatch/datafactory/SampleEntityFactory.java
+++ pro00-mssql-01-jdbc-batch/src/main/java/org/tnmk/practicespringjpa/pro00mssql01jdbcbatch/datafactory/SampleEntityFactory.java
@@ -3,11 +3,13 @@ package org.tnmk.practicespringjpa.pro00mssql01jdbcbatch.datafactory;
 import org.tnmk.practicespringjpa.pro00mssql01jdbcbatch.entity.SampleEntity;
 
 import java.time.ZonedDateTime;
+import java.util.UUID;
 
 public class SampleEntityFactory {
   public static SampleEntity constructSampleEntity() {
     SampleEntity sampleEntity = new SampleEntity();
     sampleEntity.setName("Sample_" + System.nanoTime());
+    sampleEntity.setEntityCode("C" + UUID.randomUUID());
     sampleEntity.setStartingDateTime(ZonedDateTime.now());
     return sampleEntity;
   }
