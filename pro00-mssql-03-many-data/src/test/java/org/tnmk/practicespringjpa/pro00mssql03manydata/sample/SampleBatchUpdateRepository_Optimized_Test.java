e3c447b6e3389ddc1f8742693e33ad1715e0e756
Khoi Tran <khoi.tnm@gmail.com>
2023-02-01 07:12:15 -0500

    Create a separate test case for updating error items.

--- pro00-mssql-03-many-data/src/test/java/org/tnmk/practicespringjpa/pro00mssql03manydata/sample/SampleBatchUpdateRepository_Optimized_Test.java
+++ pro00-mssql-03-many-data/src/test/java/org/tnmk/practicespringjpa/pro00mssql03manydata/sample/SampleBatchUpdateRepository_Optimized_Test.java
@@ -2,6 +2,7 @@ package org.tnmk.practicespringjpa.pro00mssql03manydata.sample;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.tnmk.practicespringjpa.pro00mssql03manydata.testinfra.BaseSpringTest_WithActual_MsSqlServer;
 
@@ -27,7 +28,17 @@ public class SampleBatchUpdateRepository_Optimized_Test extends BaseSpringTest_W
         List<SampleEntity> itemsToBeUpdated =
             changeEntityNameService.changeRandomNamesAndEntityCodesForTopItems("Approach04B_name", "Approach04B_code", UPDATE_COUNTS);
 
+        sampleBatchUpdateRepositoryOptimized.updateNamesForEntities_Approach04(itemsToBeUpdated);
+        log.info("itemsToBeUpdated: \n{}", itemsToBeUpdated.size());
+    }
+
+    @Test
+    public void updateTopItems_Approach04B_withSomeErrorItems() {
+        List<SampleEntity> itemsToBeUpdated =
+            changeEntityNameService.changeRandomNamesAndEntityCodesForTopItems_withSomeErrorItems("Approach04B_name", "Approach04B_code", UPDATE_COUNTS);
+
         UpdateResult result = sampleBatchUpdateRepositoryOptimized.updateNamesForEntities_Approach04B_withReportedResultForEachGroup(itemsToBeUpdated);
         log.info("itemsToBeUpdated: \n{}", result);
+        Assertions.assertTrue(!result.getErrorItems().isEmpty());
     }
 }
