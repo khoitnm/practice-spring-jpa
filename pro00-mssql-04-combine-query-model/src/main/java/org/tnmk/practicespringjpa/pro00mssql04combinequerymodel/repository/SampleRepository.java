c42b8e5eb42c30438668659567b6b86f7c53ec33
trank <Kevin.Tran@pointclickcare.com>
2023-09-29 10:24:18 -0400

    pro00-mssql-04-combine-query-model: Try Projection but doesn't work.

--- pro00-mssql-04-combine-query-model/src/main/java/org/tnmk/practicespringjpa/pro00mssql04combinequerymodel/repository/SampleRepository.java
+++ pro00-mssql-04-combine-query-model/src/main/java/org/tnmk/practicespringjpa/pro00mssql04combinequerymodel/repository/SampleRepository.java
@@ -3,6 +3,7 @@ package org.tnmk.practicespringjpa.pro00mssql04combinequerymodel.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.tnmk.practicespringjpa.pro00mssql04combinequerymodel.entity.CombinedModel;
+import org.tnmk.practicespringjpa.pro00mssql04combinequerymodel.entity.CombinedModel_Projection;
 import org.tnmk.practicespringjpa.pro00mssql04combinequerymodel.entity.SampleEntity;
 
 import java.util.List;
@@ -18,8 +19,22 @@ public interface SampleRepository extends JpaRepository<SampleEntity, Long> {
         ") FROM SampleEntity s JOIN s.parent p")
     List<CombinedModel> getCombinedModels();
 
-//    @Query("SELECT "
-//        + " s.id, s.name, s.parent.id as parentId, s.parent.name as parentName, s.createdDateTime " +
-//        " FROM SampleEntity s")
-//    List<CombinedModel> getCombinedModels();
+    /**
+     * Please view {@link CombinedModel_Projection} for more details about this approach.
+     * This doesn't work on old Spring Boot framework (2.5.0).
+     * So, I'm trying with newer Spring version.
+     *
+     * I tried with Spring Boot 2.7.16 (which is newest version at 2023/09/29) for Java 11.
+     * Didn't work too, maybe it only works with Spring 3.x.x (for Java 17+ only)???
+     *
+     * Anyway, I just give up this approach.
+     */
+    @Query("SELECT " +
+        " s.id," +
+        " s.name," +
+        " p.id as parentId," +
+        " p.name as parentName," +
+        " s.createdDateTime " +
+        " FROM SampleEntity s JOIN s.parent p")
+    List<CombinedModel_Projection> getCombinedModelsUseProjection();
 }
--- pro00-mssql-04-combine-query-model/src/test/java/org/tnmk/practicespringjpa/pro00mssql04combinequerymodel/samplebusiness/SampleBusinessTest.java
+++ pro00-mssql-04-combine-query-model/src/test/java/org/tnmk/practicespringjpa/pro00mssql04combinequerymodel/samplebusiness/SampleBusinessTest.java
@@ -2,20 +2,19 @@ package org.tnmk.practicespringjpa.pro00mssql04combinequerymodel.samplebusiness;
 
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
-import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.tnmk.practicespringjpa.pro00mssql04combinequerymodel.datafactory.SampleEntityFactory;
 import org.tnmk.practicespringjpa.pro00mssql04combinequerymodel.entity.CombinedModel;
+import org.tnmk.practicespringjpa.pro00mssql04combinequerymodel.entity.CombinedModel_Projection;
 import org.tnmk.practicespringjpa.pro00mssql04combinequerymodel.entity.ParentEntity;
 import org.tnmk.practicespringjpa.pro00mssql04combinequerymodel.entity.SampleEntity;
 import org.tnmk.practicespringjpa.pro00mssql04combinequerymodel.repository.ParentRepository;
 import org.tnmk.practicespringjpa.pro00mssql04combinequerymodel.repository.SampleRepository;
-import org.tnmk.practicespringjpa.pro00mssql04combinequerymodel.story.SampleStory;
 import org.tnmk.practicespringjpa.pro00mssql04combinequerymodel.testinfra.BaseSpringTest;
 
 import java.util.List;
-import java.util.Optional;
 
 @Slf4j
 public class SampleBusinessTest extends BaseSpringTest {
@@ -24,19 +23,45 @@ public class SampleBusinessTest extends BaseSpringTest {
     @Autowired
     private ParentRepository parentRepository;
 
-    @Test
-    public void test() {
+    private SampleEntity initSampleDataWithParent() {
         ParentEntity parentEntity = new ParentEntity();
         parentEntity.setName("parent");
         parentRepository.save(parentEntity);
 
         SampleEntity sampleEntity = SampleEntityFactory.constructSampleEntity(parentEntity);
         SampleEntity savedSampleEntity = sampleRepository.save(sampleEntity);
+        return savedSampleEntity;
+    }
+
+    @Test
+    public void test_getCombinedModels_successfully() {
+        initSampleDataWithParent();
 
         List<CombinedModel> combinedModels = sampleRepository.getCombinedModels();
+
         Assertions.assertThat(combinedModels).isNotEmpty();
-        log.info("CombinedModels: "+combinedModels);
+        log.info("CombinedModels: " + combinedModels);
         for (CombinedModel combinedModel : combinedModels) {
+            Assertions.assertThat(combinedModel.getId()).isNotNull();
+            Assertions.assertThat(combinedModel.getName()).isNotBlank();
+            Assertions.assertThat(combinedModel.getParentName()).isNotBlank();
+            Assertions.assertThat(combinedModel.getParentId()).isNotNull();
+        }
+    }
+
+    /**
+     * @see SampleRepository#getCombinedModelsUseProjection() for more details about why it is disabled!!!
+     */
+    @Test
+    @Ignore
+    public void test_getCombinedModelsUsingProjection_successfully() {
+        initSampleDataWithParent();
+
+        List<CombinedModel_Projection> combinedModels = sampleRepository.getCombinedModelsUseProjection();
+
+        Assertions.assertThat(combinedModels).isNotEmpty();
+        log.info("CombinedModels: " + combinedModels);
+        for (CombinedModel_Projection combinedModel : combinedModels) {
             Assertions.assertThat(combinedModel.getId()).isNotNull();
             Assertions.assertThat(combinedModel.getName()).isNotBlank();
             Assertions.assertThat(combinedModel.getParentName()).isNotBlank();
