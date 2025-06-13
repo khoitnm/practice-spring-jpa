e3acee7c973dbd15335e610446086ae18c44ff0b
Khoi Tran <khoi.tnm@gmail.com>
2025-06-13 16:34:02 -0400

    Report slow tenants.

--- pro07-multi-tenant-row-level/src/test/java/org/tnmk/practice_spring_jpa/pro07_multi_tenant_row_level/business/MultiTenantConnectionProviderStressTest.java
+++ pro07-multi-tenant-row-level/src/test/java/org/tnmk/practice_spring_jpa/pro07_multi_tenant_row_level/business/MultiTenantConnectionProviderStressTest.java
@@ -6,9 +6,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.StopWatch;
 import org.tnmk.practice_spring_jpa.pro07_multi_tenant_row_level.common.multitenant.MultiTenantConnectionProviderImpl;
 
 import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -16,6 +19,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 @SpringBootTest
 class MultiTenantConnectionProviderStressTest {
 
+    public static final int SLOW_TENANT_THRESHOLD_MILLS = 200;
     @Autowired
     private MultiTenantConnectionProviderImpl multiTenantConnectionProvider;
 
@@ -25,10 +29,11 @@ class MultiTenantConnectionProviderStressTest {
     void stressTest() throws InterruptedException {
         // GIVEN
         final AtomicInteger errorsCount = new AtomicInteger(0);
-        int numberOfThreads = 30;
-        int tenantsPerThread = 10;
+        // When increase the number of threads to 50, tenantsPerThread to 100, some connection will get request timeout.
+        int numberOfThreads = 50;
+        int tenantsPerThread = 100;
         CountDownLatch latch = new CountDownLatch(numberOfThreads);
-
+        List<String> slowTenants = new ArrayList<>();
         // WHEN
         for (int i = 0; i < numberOfThreads; i++) {
             final int threadIndex = i;
@@ -36,7 +41,8 @@ class MultiTenantConnectionProviderStressTest {
                 try {
                     for (int j = 0; j < tenantsPerThread; j++) {
                         String tenantId = "stressTenant-%s-%s".formatted(threadIndex, j);
-
+                        StopWatch stopWatch = new StopWatch();
+                        stopWatch.start(tenantId);
                         try (Connection connection = multiTenantConnectionProvider.getConnection(tenantId);) {
                             /**
                              * Just closing connection alone is not enough, we need to call `releaseConnection` to REVERT the user context (set by `EXECUTED AS...`) from the connection.
@@ -47,6 +53,12 @@ class MultiTenantConnectionProviderStressTest {
                         } catch (Exception e) {
                             errorsCount.incrementAndGet();
                             log.error("Error while getting connection for tenant: {}", tenantId, e);
+                        }
+                        stopWatch.stop();
+                        log.info("Finished getting connection for tenant {} in {} ms", tenantId, stopWatch.getTotalTimeMillis());
+                        if (stopWatch.getTotalTimeMillis() > SLOW_TENANT_THRESHOLD_MILLS) {
+                            slowTenants.add(tenantId + " took " + stopWatch.getTotalTimeMillis() + " ms");
+                            log.warn("Tenant {} took too long to get and release connection: {} ms", tenantId, stopWatch.getTotalTimeMillis());
                         }
                     }
                 } catch (Exception e) {
@@ -62,6 +74,9 @@ class MultiTenantConnectionProviderStressTest {
         latch.await(); // Wait for all threads to finish
         log.info("All threads have completed execution.");
         Assertions.assertThat(errorsCount.get()).as("There should be no error when getting connection for tenant.").isEqualTo(0);
+        log.info("Slow tenants: " + slowTenants.size() + " \n"
+            + String.join(",\n", slowTenants)
+        );
     }
 
 }
