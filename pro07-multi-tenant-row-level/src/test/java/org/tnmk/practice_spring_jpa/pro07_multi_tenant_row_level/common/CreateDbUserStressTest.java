3f0f0831fc6104c148fd0bd2a2d1b6b2aa3d8789
Khoi Tran <khoi.tnm@gmail.com>
2025-06-16 08:43:36 -0400

    Refactor StressTestHelper for MultiTenantConnectionProviderStressTest

--- pro07-multi-tenant-row-level/src/test/java/org/tnmk/practice_spring_jpa/pro07_multi_tenant_row_level/common/CreateDbUserStressTest.java
+++ pro07-multi-tenant-row-level/src/test/java/org/tnmk/practice_spring_jpa/pro07_multi_tenant_row_level/common/CreateDbUserStressTest.java
@@ -27,14 +27,11 @@ class CreateDbUserStressTest {
     @DisplayName("This test will show how MultiTenantConnectionProviderImpl can handle many parallel requests at the same time." +
         "Right now, it's getting dead lock because MS SQL Server cannot handle too many DDL in parallel (it may get dead lock, and lead to connection timeout), will need to improve.")
     @Test
-    void stressTest() throws InterruptedException {
-        StressTestResult result = StressTestHelper.run(5, 2, new StressTestHelper.ThrowingBiConsumer<Integer, Integer>() {
-            @Override
-            public void accept(Integer threadIndex, Integer loopIndex) throws Exception {
-                String dbUsername = "stressDbUserC-%s-%s".formatted(threadIndex, loopIndex);
-                try (Connection connection = dataSource.getConnection();) {
-                    dbUserService.createDbUser(connection, dbUsername);
-                }
+    void stressTest() {
+        StressTestResult result = StressTestHelper.run(5, 2, (threadIndex, loopIndex) -> {
+            String dbUsername = "stressDbUserC-%s-%s".formatted(threadIndex, loopIndex);
+            try (Connection connection = dataSource.getConnection();) {
+                dbUserService.createDbUser(connection, dbUsername);
             }
         });
 
--- pro07-multi-tenant-row-level/src/test/java/org/tnmk/practice_spring_jpa/pro07_multi_tenant_row_level/common/MultiTenantConnectionProviderStressTest.java
+++ pro07-multi-tenant-row-level/src/test/java/org/tnmk/practice_spring_jpa/pro07_multi_tenant_row_level/common/MultiTenantConnectionProviderStressTest.java
@@ -1,19 +1,17 @@
 package org.tnmk.practice_spring_jpa.pro07_multi_tenant_row_level.common;
 
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.util.StopWatch;
 import org.tnmk.practice_spring_jpa.pro07_multi_tenant_row_level.common.multitenant.MultiTenantConnectionProviderImpl;
+import org.tnmk.practice_spring_jpa.pro07_multi_tenant_row_level.support_for_testing.StressTestHelper;
+import org.tnmk.practice_spring_jpa.pro07_multi_tenant_row_level.support_for_testing.StressTestResult;
 
 import java.sql.Connection;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
 @SpringBootTest
@@ -26,58 +24,19 @@ class MultiTenantConnectionProviderStressTest {
     @DisplayName("This test will show how MultiTenantConnectionProviderImpl can handle many parallel requests at the same time." +
         "Right now, it's getting dead lock because MS SQL Server cannot handle too many DDL in parallel (it may get dead lock, and lead to connection timeout), will need to improve.")
     @Test
-    void stressTest() throws InterruptedException {
-        // GIVEN
-        final AtomicInteger errorsCount = new AtomicInteger(0);
-        // When increase the number of threads to 50, tenantsPerThread to 100, some connection will get request timeout.
-        int numberOfThreads = 50;
-        int tenantsPerThread = 100;
-        CountDownLatch latch = new CountDownLatch(numberOfThreads);
-        List<String> slowTenants = new ArrayList<>();
-        // WHEN
-        for (int i = 0; i < numberOfThreads; i++) {
-            final int threadIndex = i;
-            new Thread(() -> {
-                try {
-                    for (int j = 0; j < tenantsPerThread; j++) {
-                        String tenantId = "stressTenant-%s-%s".formatted(threadIndex, j);
-                        StopWatch stopWatch = new StopWatch();
-                        stopWatch.start(tenantId);
-                        try (Connection connection = multiTenantConnectionProvider.getConnection(tenantId);) {
-                            /**
-                             * Just closing connection alone is not enough, we need to call `releaseConnection` to REVERT the user context (set by `EXECUTED AS...`) from the connection.
-                             * If we don't do that, it will get error for the next thread (please view more in {@link MultiTenantConnectionProviderImpl#releaseAnyConnection(Connection)}.
-                             * Question: what if business level get some error, can it REVERT the connection?
-                             */
-                            multiTenantConnectionProvider.releaseConnection(tenantId, connection);
-                        } catch (Exception e) {
-                            errorsCount.incrementAndGet();
-                            log.error("Error while getting connection for tenant: {}", tenantId, e);
-                        }
-                        stopWatch.stop();
-                        log.info("Finished getting connection for tenant {} in {} ms", tenantId, stopWatch.getTotalTimeMillis());
-                        if (stopWatch.getTotalTimeMillis() > SLOW_TENANT_THRESHOLD_MILLS) {
-                            slowTenants.add(tenantId + " took " + stopWatch.getTotalTimeMillis() + " ms");
-                            log.warn("Tenant {} took too long to get and release connection: {} ms", tenantId, stopWatch.getTotalTimeMillis());
-                        }
-                    }
-                } catch (Exception e) {
-                    log.error("Error in thread {}: {}", threadIndex, e.getMessage(), e);
-                    errorsCount.incrementAndGet();
-                } finally {
-                    latch.countDown();
-                }
-            }).start();
-        }
-
-        // THEN
-        latch.await(); // Wait for all threads to finish
-        log.info("All threads have completed execution.");
-        log.info("Slow tenants: " + slowTenants.size() + " \n"
-            + String.join(",\n", slowTenants)
-        );
-
-        Assertions.assertThat(errorsCount.get()).as("There should be no error when getting connection for tenant.").isEqualTo(0);
+    void stressTest() {
+        StressTestResult result = StressTestHelper.run(30, 1000, (threadIndex, loopIndex) -> {
+            String tenantId = "stressTenant-%s-%s".formatted(threadIndex, loopIndex);
+            try (Connection connection = multiTenantConnectionProvider.getConnection(tenantId);) {
+                /**
+                 * Just closing connection alone is not enough, we need to call `releaseConnection` to REVERT the user context (set by `EXECUTED AS...`) from the connection.
+                 * If we don't do that, it will get error for the next thread (please view more in {@link MultiTenantConnectionProviderImpl#releaseAnyConnection(Connection)}.
+                 * Question: what if business level get some error, can it REVERT the connection?
+                 */
+                multiTenantConnectionProvider.releaseConnection(tenantId, connection);
+            }
+        });
+        assertThat(result.getErrorCount()).as("There should be no error when getting connection for tenant.").isEqualTo(0);
     }
 
 }
